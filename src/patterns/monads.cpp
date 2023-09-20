c50a847f5aa612583d13d2596dad0c8408474415
Chris <143294640+Chris2Rich@users.noreply.github.com>
2023-09-20 08:06:12 +0100

    ahhhh

--- src/patterns/monads.cpp
+++ src/patterns/monads.cpp
@@ -3,21 +3,37 @@
 class OperateVal{
 private:
     float val;
+    struct ReturnVal{
+        float vals[4];
+        ReturnVal(float inpa, float inpb, float inpc, float inpd){
+            vals[0] = inpa;
+            vals[1] = inpb;
+            vals[2] = inpc;
+            vals[3] = inpd;
+            
+        float operator[](int i){
+            return (i < 4) ? vals[i] : 0;
+            }
+        }
+    };
 public:
 
     OperateVal(float inp){
         val = inp;
     }
 
-    float* operator()(float inp){
-        float ans[4] = {inp+val, inp-val, inp*val, inp/val};
+    ReturnVal operator()(float inp){
+        ReturnVal ans(inp+val, inp-val, inp*val, inp/val);
         return ans;
     }
 };
 
-void map(){
-    
-}
+class Map{
+    template <typename T>
+    Map(T inp){
+        return Map<inp>;
+    }
+};
 
 int main(){
     auto fn = OperateVal(2);
