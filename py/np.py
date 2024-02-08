9133a2bbfb8e6915f2401a45d3a17024f78dad9e
Chris2Rich <AzkronTheGamer@gmail.com>
2024-02-08 19:24:46 +0000

    .

--- py/np.py
+++ py/np.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-x = np.array([[1,2,3],
-              [4,5,6],
-              [7,8,9]])
-y = np.array([[1,3],
-              [4,6]])
+def MSE(data, func):
+    return ((np.vectorize(lambda x: x*x))(np.vectorize(func)(np.array(list(range(0,data.size)))) - data)).mean()
 
-print(y in x)
+data = np.array([1,3,6,5,6,12,24])
+func = lambda x: x + 2
+
+print(MSE(data, func))
