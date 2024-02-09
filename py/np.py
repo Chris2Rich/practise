9d1a5d8c887773fe91ca4f804eae01a3f7871e3f
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-02-09 08:24:53 +0000

    Update np.py

--- py/np.py
+++ py/np.py
@@ -1,9 +1,11 @@
 import numpy as np
+import timeit
 
-def MSE(data, func):
+def mse(data, func):
     return ((np.vectorize(lambda x: x*x))(np.vectorize(func)(np.array(list(range(0,data.size)))) - data)).mean()
+    
+def variance(data):
+    return (np.vectorize(lambda x: x*x)(np.array([data.mean()]*data.size) - data)).sum() / data.size
 
-data = np.array([1,3,6,5,6,12,24])
+data = np.array([1,2,3,6])
 func = lambda x: x + 2
-
-print(MSE(data, func))
