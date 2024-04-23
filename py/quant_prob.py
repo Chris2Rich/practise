81f03572d8288d1aa16f9122952eab408e1a7546
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-04-23 18:05:44 +0100

    Update quant_prob.py

--- py/quant_prob.py
+++ py/quant_prob.py
@@ -1,10 +1,10 @@
 # Recursion as solution to number of trials for discrete memoryless random function to return same value 1/px times
-px = 1/2
+px = 1/3
 
 def prob(n):
     if n == 1:
         return 1/px
     else:
-        return prob(n-1) + 1/(px ** n)
+        return int(prob(n-1) + 1/(px ** n))
         
 print(prob(5))
