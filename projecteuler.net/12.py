78cca76291a89f37438f920f0638830772783e5d
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-10-15 14:13:51 +0100

    partial solution?

--- projecteuler.net/12.py
+++ projecteuler.net/12.py
@@ -1,2 +1,10 @@
-triangular = lambda x: sum(i for i in range(1, x+1))
-factorize = lambda
+triangular = lambda x: 0.5 * x * (x+1)
+factor_count = lambda x: sum(x % i == 0 for i in range(1, int(x ** 0.5)+ 1)) * 2 - (x ** 0.5 == int(x ** 0.5))
+
+n = 1
+a = factor_count(triangular(n))
+while a < 500:
+    n *= 2
+    a = factor_count(triangular(n))
+
+print(n)
