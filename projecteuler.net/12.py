c26bc0ea600b1c5a278ce1b72ebb836574da1356
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-10-15 14:14:46 +0100

    Update 12.py

--- projecteuler.net/12.py
+++ projecteuler.net/12.py
@@ -4,7 +4,7 @@ factor_count = lambda x: sum(x % i == 0 for i in range(1, int(x ** 0.5)+ 1)) * 2
 n = 1
 a = factor_count(triangular(n))
 while a < 500:
-    n *= 2
+    n += 1
     a = factor_count(triangular(n))
 
 print(n)
