03dfdc6958268bc1132559ef9d6f74f7278842ab
Chris <143294640+Chris2Rich@users.noreply.github.com>
2024-12-17 14:44:32 +0000

    ?

--- cryptographic_hash_functions/complex_elliptic_curves.cpp
+++ cryptographic_hash_functions/complex_elliptic_curves.cpp
@@ -8,20 +8,25 @@ struct cp /*complex point */{
     cp(int real, int imaginary){
         r = real;
         i = imaginary;
-    }
+    };
 
     cp operator+(cp n){
         r = (r + n.r) % m;
         i = (i + n.i) % m;
-    }
+        return *this;
+    };
 
     cp operator-(cp n){
         r = (r - n.r) % m;
         i = (i - n.i) % m;
-    }
-}
+        return *this;
+    };
+};
 
 int main(){
-
+    cp a(1,2);
+    cp b(3, -5);
+    a = a - b;
+    std::cout << a.i;
     return 0;
 }
