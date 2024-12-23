f724a5bf530f2692546ebe90cdd18bae994e12c9
Chris2Rich <AzkronTheGamer@gmail.com>
2024-12-23 08:32:36 +0000

    for cf?

--- IOI Practise/codeforces/550C.py
+++ IOI Practise/codeforces/550C.py
@@ -1,24 +1,28 @@
-n = "4352634623652765264656234569324572634659164656314654236526395642365692344514306546125691364563146754233465161234654"
+n = input()
 
 dig2 = [str(i) if i > 10 else "0" + str(i) for i in range(0, 100, 8)]
 dig3 = [str(i) if i > 100 else "0" + str(i) if i > 10 else "00" + str(i) for i in range(0, 1000, 8)]
 
 def wun():
     if n == "0" or n == "8":
-        print("YES : ", n)
+        print("YES")
+        print(n)
     else:
         print("NO")
 
 def tu():
     if n in dig2:
-        print("YES : ", n)
+        print("YES")
+        print(n)
         return
 
     if "0" in n or "8" in n:
         if n.find("0") != -1:
-            print("YES : 0")
+            print("YES")
+            print("0")
         else:
-            print("Yes : 8")
+            print("Yes")
+            print("8")
     else:
         print("NO")
 
@@ -33,7 +37,11 @@ def trey():
                 ix.append(n.find(j,ix[-1]+1))
         if (ix[0] < ix[1] and ix[1] < ix[2]) and (ix[0] != -1 and ix[1] != -1 and ix[2] != -1):
             div = True
-            print("YES : ", i)
+            print("YES")
+            if i == "000":
+                print("0")
+            else:
+                print(i.lstrip("0"))
             return
     if div == False:
         print("NO")
