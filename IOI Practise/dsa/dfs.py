8edd4f9c5c6f6d5e294ad0572bc23febdb6d9d66
Chris2Rich <AzkronTheGamer@gmail.com>
2024-12-30 18:46:34 +0000

    !

--- IOI Practise/dsa/dfs.py
+++ IOI Practise/dsa/dfs.py
@@ -14,17 +14,13 @@ pos = 0
 
 def dfs(t: int, head: Node):
     global pos
+    if head in seen:
+        return -1
 
     if head.val == t:
         return pos
     else:
+        seen.update({str(head):True})
         pos += 1
         res = list(filter(lambda x: x != -1, [dfs(t, i) for i in head.edges]))
         return min(res) if res != [] else -1
-
-a = Node(3)
-b = Node(6, [a])
-c = Node(7, [a,b])
-d = Node(2, [c])
-
-print(dfs(2,d))
