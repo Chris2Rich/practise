00a718e013f58306edf074d001326d91e472a59a
Chris2Rich <AzkronTheGamer@gmail.com>
2024-12-30 14:56:34 +0000

    edit

--- cryptographic_hash_functions/complex_elliptic_curves.cpp
+++ cryptographic_hash_functions/complex_elliptic_curves.cpp
@@ -1,32 +1,11 @@
 #include <iostream>
 
-struct cp /*complex point */{
-    long int r; //real component
-    long int i; //imaginary component
+struct cp /*point */{
+    long int x; //x component
+    long int y; //y component
     long int m; //modulo
-
-    cp(int real, int imaginary){
-        r = real;
-        i = imaginary;
-    };
-
-    cp operator+(cp n){
-        r = (r + n.r) % m;
-        i = (i + n.i) % m;
-        return *this;
-    };
-
-    cp operator-(cp n){
-        r = (r - n.r) % m;
-        i = (i - n.i) % m;
-        return *this;
-    };
 };
 
 int main(){
-    cp a(1,2);
-    cp b(3, -5);
-    a = a - b;
-    std::cout << a.i;
     return 0;
 }
