b0792c7cbb576f41de527a8ba989f0725416d1b5
Chris2rich <cz07business@gmail.com>
2025-09-03 10:42:42 +0100

    cool asf

--- py/fractals/julia.py
+++ py/fractals/julia.py
@@ -10,15 +10,17 @@ def julia(z:complex, c: complex, limit: int=5):
 
 # Example usage
 if __name__ == "__main__":
-    limit = 100
-    width, height = 1920, 1080
+    limit = int(input("Input the limit:\t"))
+    width, height = int(input("Input width of result image:\t")), int(input("Input height of result image:\t"))
+    C = complex(input("Input complex (1+1j format):\t"))
+    
     x = np.linspace(-1.5, 1.5, width)
     y = np.linspace(-1.5, 1.5, height)
     X, Y = np.meshgrid(x, y)
     Z = X + (1j * Y)
 
-    c = complex(-0.5125 , 0.5213)
-    vjulia = np.vectorize(lambda z: julia(z,c, limit=limit))
+    vjulia = np.vectorize(lambda z: julia(z,C, limit=limit))
     julia_vals = vjulia(Z)
 
-    array_to_picture(julia_vals, cmap="magma", save_path=f"./py/fractals/pictures/julia{str(c)}{limit}.png")
+    array_to_picture(julia_vals, cmap="magma", save_path=f"./py/fractals/pictures/julia{str(C)}{limit}.png")
+    print(f"Saved at ./py/fractals/pictures/julia{str(C)}{limit}.png")
--- py/fractals/mandlebrot.py
+++ py/fractals/mandlebrot.py
@@ -11,9 +11,10 @@ def mandlebrot(c: complex, limit: int=5):
 
 # Example usage
 if __name__ == "__main__":
-    limit = 100
+    limit = int(input("Input the limit:\t"))
+    width, height = int(input("Input width of result image:\t")), int(input("Input height of result image:\t"))
+
     vmandelbrot = np.vectorize(mandlebrot)
-    width, height = 3840, 2160
     x = np.linspace(-2.0, 1.0, width)
     y = np.linspace(-1.25, 1.25, height)
     X, Y = np.meshgrid(x, y)
@@ -21,3 +22,4 @@ if __name__ == "__main__":
 
     mandelbrot_vals = vmandelbrot(C, limit=limit)
     array_to_picture(mandelbrot_vals, cmap="magma", save_path=f"./py/fractals/pictures/mandlebrot{limit}.png")
+    print(f"Saved at ./py/fractals/pictures/mandlebrot{limit}.png")
