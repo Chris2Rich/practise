ceea1a05cb12c827c735495c388e6488304a7930
Chris2rich <cz07business@gmail.com>
2025-11-18 11:48:18 +0000

    !

--- py/deletek.py
+++ py/deletek.py
@@ -1,12 +1,24 @@
-def solve(s, k):
+def solve_recur(s, k):
     if k == 0:
         return s
     if len(s) == k:
         return ""
-    if int(s[0]) < int(s[1]):
-        return solve(s[1:], k-1)
     if int(s[0]) < max([int(i) for i in s[:k+1]]):
-        return solve(s[1:], k-1)
-    return s[0] + solve(s[1:], k)
+        return solve_recur(s[1:], k-1)
+    return s[0] + solve_recur(s[1:], k)
 
-print([solve("7396350874", i+1) for i in range(10)])
+def solve_iter(s, k):
+    res = ""
+    while k >= 0:
+        if len(s) == k:
+            return res
+        if int(s[0]) < max([int(i) for i in s[:k+1]]):
+            s = s[1:]
+            k -= 1
+        else:
+            res += s[0]
+            s = s[1:]
+    return res
+
+print([solve_iter("7396350874", i+1) for i in range(10)])
+print([solve_recur("7396350874", i+1) for i in range(10)])
